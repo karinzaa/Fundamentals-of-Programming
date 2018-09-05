f4da824a6a5fd887fb0485755ad28fc4a3c583f4
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2018-09-06 00:38:27 +0700

    Update 1

---
+++ Fundamentals-of-Programming/LabWeek2/BonusLab1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("The first letter of the alphabet is %c", 'a');
+	printf("\nThe decimal code for this letter is %d", 'a');
+	printf("\nThe code for an uppercase %c is %d\n", 'A', 'A');
+	
+	printf("My letter#1 is %c", 'x');
+	printf("\nThe decimal code for this letter is %d", 'x');
+	printf("\nThe code for an uppercase %c is %d\n", 'X', 'X');
+	
+	printf("My letter#2 is %c", 'y');
+	printf("\nThe decimal code for this letter is %d", 'y');
+	printf("\nThe code for an uppercase %c is %d\n", 'Y', 'Y');
+	
+	printf("My letter#3 is %c", 'z');
+	printf("\nThe decimal code for this letter is %d", 'z');
+	printf("\nThe code for an uppercase %c is %d\n", 'Z', 'Z');
+	
+	return 0;
+}
---
+++ Fundamentals-of-Programming/LabWeek2/Lab2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	float m,y;
+		printf("My weight is ");
+		scanf("%f",&m);
+		printf("You weight are ");
+		scanf("%f",&y);
+		printf("\tWe are around %.2f",(m+y)/2);
+	return 0;
+}
